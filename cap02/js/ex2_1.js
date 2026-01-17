aecf04d81c9f4c11e608588cf68a8cfeae11ae01
Thiago Telheiro <150066861+ThiagoTelheiro@users.noreply.github.com>
2026-01-17 15:55:04 -0300

    Add files via upload

---
+++ cap02/js/ex2_1.js
@@ -0,0 +1,11 @@
+//Cria uma referencia ao form e ao elemento h3 (onde será exibida a resposta)
+const frm = document.querySelector("form");
+const resp = document.querySelector("h3");
+
+//Cria um "ouvinte" de evento, acionado quando o botão submit for clicado
+frm.addEventListener("submit", (e) => {
+  const nome = frm.inNome.value; // obtém o nome digitado no from
+  resp.innerText = `Olá ${nome}`; //exibe a resposta do progama
+
+  e.preventDefault(); // evita o envio do form
+});
