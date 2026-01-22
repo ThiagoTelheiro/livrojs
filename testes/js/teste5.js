81a3ffb5519d312e1002c856806b6990283dc59a
Thiago Telheiro <150066861+ThiagoTelheiro@users.noreply.github.com>
2026-01-21 22:54:31 -0300

    Add files via upload

---
+++ testes/js/teste5.js
@@ -0,0 +1,17 @@
+const prompt = require("prompt-sync")();
+
+// let numeros = Number(prompt("Digite Valores:"));
+// let soma = 0;
+
+// let para = 1;
+
+// while (para != 0) {
+//   soma = soma + numeros;
+//   numeros = Number(prompt("Digite Valores:"));
+//   if (numeros == 0) {
+//     para = 0;
+//   }
+// }
+
+// console.log(`soma: ${soma}`);
+
