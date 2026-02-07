22f48cc1f41712ec09425a4e07fbee2e93047a47
Thiago Telheiro <150066861+ThiagoTelheiro@users.noreply.github.com>
2026-02-07 15:43:09 -0300

    Add files via upload

--- cap04/js/resp4_c.js
+++ cap04/js/resp4_c.js
@@ -6,7 +6,6 @@ frm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const valorD = Number(frm.inNum.value);
-  console.log(valorD);
 
   const min30 = 1.0;
   const min60 = 1.75;
@@ -14,21 +13,23 @@ frm.addEventListener("submit", (e) => {
 
   if (valorD < min30) {
     resp1.innerText = `Valor Insuficiente`;
+    resp2.innerText = ``;
+    frm.inNum.focus();
+    return;
   }
 
   let troco;
-
-  if (valorD >= min30) {
-    troco = valorD - min30;
-    resp1.innerText = `Tempo: 30 min`;
+  if (valorD >= min120) {
+    troco = valorD - min120;
+    resp1.innerText = `Tempo: 120min`;
     resp2.innerText = `Troco: ${troco.toFixed(2)}`;
   } else if (valorD >= min60) {
     troco = valorD - min60;
-    resp1.innerText = `Tempo: 60 min`;
+    resp1.innerText = `Tempo: 60min`;
     resp2.innerText = `Troco: ${troco.toFixed(2)}`;
   } else {
-    troco = valorD - min120;
-    resp1.innerText = `Tempo: 120 min`;
+    troco = valorD - min30;
+    resp1.innerText = `Tempo: 30min`;
     resp2.innerText = `Troco: ${troco.toFixed(2)}`;
   }
 });
