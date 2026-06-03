15f4c1abca776cca9cde85ec2f03b923060c1a92
Thiago Telheiro <150066861+ThiagoTelheiro@users.noreply.github.com>
2026-06-03 11:16:01 -0300

    Add files via upload

---
+++ ex5_10.js
@@ -0,0 +1,13 @@
+const prompt = require("prompt-sync")();
+
+const valor = Number(prompt("Valor: "));
+const num = Number(prompt("N° de Parcelas: "));
+
+const valorParcelas = Math.floor(valor / num);
+const valorFinal = valorParcelas + valor % num;
+
+for (let i = 1; i < num; i++) {
+  console.log(`${i}° parcela: R$ ${valorParcelas.toFixed(2)} `);
+}
+
+console.log(`${num}° parcela: R$ ${valorFinal.toFixed(2)}`);
